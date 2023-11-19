80a2de27880da90086d678f8d6b8f710e177a0a9
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-19 09:48:26 +0530

    Allowing Keyboards Inputs (Enter Key)

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
           ref={inputTask}
           type="text"
           placeholder="Task...."
+          onKeyDown={(event) => {if (event.keyCode == 13) addTask()}}
           onChange={(event) => {
             setCurrentTask(event.target.value);
           }}
