a3383d89cc867019e0c60d0f044ecf868e2a1163
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-19 09:24:00 +0530

    Clearing the input after adding a task

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
 
   const [todoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState("");
 
+  const inputTask = useRef(null)
+
 
   const addTask = () => {
     setTodoList([...todoList, currentTask]);
+    inputTask.current.value = "";
+    setCurrentTask("");
   }
 
   return (
     <div className="App">
       <h1>Todo List</h1>
       <div>
-        <input type="text" placeholder='Task....' onChange={(event) => {
+        <input ref={inputTask} type="text" placeholder='Task....' onChange={(event) => {
           setCurrentTask(event.target.value);
         }}/> 
         <button onClick={addTask}>Add Task </button>
