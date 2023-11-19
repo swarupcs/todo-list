b6a4980132ec43bccb67e0b18c8da96af4985b07
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-19 09:22:50 +0530

    Add task and rendering list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+
+
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div>
+        <input type="text" placeholder='Task....' onChange={(event) => {
+          setCurrentTask(event.target.value);
+        }}/> 
+        <button onClick={addTask}>Add Task </button>
+      </div>
+      <hr />
+      <ul>
+        {todoList.map((val, key)=> {
+          return (
+            <li> {val} </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
