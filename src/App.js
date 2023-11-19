a446ba119856ae482c91507dd669eaa27261d5ad
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-19 09:43:37 +0530

    Complete a task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import './App.css';
-import { useState, useRef } from 'react';
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
-
   const [todoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState("");
 
-  const inputTask = useRef(null)
-
+  const inputTask = useRef(null);
 
   const addTask = () => {
-    setTodoList([...todoList, currentTask]);
+    setTodoList([...todoList, { task: currentTask, completed: false }]);
     inputTask.current.value = "";
     setCurrentTask("");
   };
 
   const deleteTask = (taskToDelete) => {
-    setTodoList(todoList.filter((task)=> {
-      return task !== taskToDelete;
-    })
+    setTodoList(
+      todoList.filter((task) => {
+        return task.task !== taskToDelete;
+      })
+    );
+  };
+
+  const completeTask = (taskToComplete) => {
+    setTodoList(
+      todoList.map((task) => {
+        return task.task == taskToComplete
+          ? { task: taskToComplete, completed: true }
+          : { task: task.task, completed: task.completed ? true : false };
+      })
     );
   };
 
@@ -26,20 +35,31 @@ function App() {
     <div className="App">
       <h1>Todo List</h1>
       <div>
-        <input ref={inputTask} type="text" placeholder='Task....' onChange={(event) => {
-          setCurrentTask(event.target.value);
-        }}/> 
+        <input
+          ref={inputTask}
+          type="text"
+          placeholder="Task...."
+          onChange={(event) => {
+            setCurrentTask(event.target.value);
+          }}
+        />
         <button onClick={addTask}>Add Task </button>
       </div>
       <hr />
       <ul>
-        {todoList.map((val, key)=> {
+        {todoList.map((val, key) => {
           return (
-            <div id='task'>
-            <li key={key}> {val} </li>
-            <button onClick={()=>deleteTask(val)}> X </button>
+            <div id="task">
+              <li key={key}> {val.task} </li>
+              <button onClick={() => completeTask(val.task)}>Completed</button>
+              <button onClick={() => deleteTask(val.task)}> X </button>
+              {val.completed ? (
+                <h1>Task Completed</h1>
+              ) : (
+                <h1>Task Not Completed</h1>
+              )}
             </div>
-          )
+          );
         })}
       </ul>
     </div>
